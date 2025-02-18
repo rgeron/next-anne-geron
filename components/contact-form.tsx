b1f356eb835d8450fa0f967185c0521ee5b8ab61
rgeron <raphaelgeron123@gmail.com>
2025-02-18 13:36:50 +0100

    contact form styling

--- components/contact-form.tsx
+++ components/contact-form.tsx
@@ -26,48 +26,39 @@ export function ContactForm() {
   return (
     <form id="contact-form" action={onSubmit} className="space-y-8">
       <div
-        className={`${agrandirRegular.className} grid grid-cols-1 md:grid-cols-2 gap-6`}
+        className={`${agrandirRegular.className}  grid grid-cols-1 md:grid-cols-2 gap-6`}
       >
         <div className="space-y-3">
-          <label htmlFor="name" className="text-base font-semibold">
-            Nom et prénom
-          </label>
           <Input
             id="name"
             name="name"
             placeholder="Votre nom"
             required
-            className="h-12 text-lg bg-white"
+            className="h-8 text-sm bg-white"
           />
         </div>
         <div className="space-y-3">
-          <label htmlFor="phone" className="text-base font-semibold">
-            Numéro de téléphone
-          </label>
           <Input
             id="phone"
             name="phone"
             placeholder="Votre numéro de téléphone"
             required
-            className="h-12 text-lg bg-white"
+            className="h-8 text-sm bg-white"
           />
         </div>
       </div>
       <div className="space-y-3">
-        <label htmlFor="message" className="text-base font-semibold">
-          Message
-        </label>
         <Textarea
           id="message"
           name="message"
           placeholder="Votre message"
-          className="min-h-[200px] text-lg bg-white"
+          className="min-h-[100px] text-lg bg-white"
           required
         />
       </div>
       <Button
         type="submit"
-        className="w-full md:w-auto text-lg h-12 px-8"
+        className="w-full md:w-auto text-base h-8 px-8 "
         disabled={isPending}
       >
         {isPending ? "Envoi en cours..." : "Envoyer"}
